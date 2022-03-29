2750803572ab3ce2a03098195eb91808305fdf3a
kellyn nettles <92842273+nellynkettles@users.noreply.github.com>
2022-03-29 16:39:00 -0400

    this should work

--- java-script-syntax/script.js
+++ java-script-syntax/script.js
@@ -1,8 +1,8 @@
 let count = 0
-let everyday = newBeginnings ();
+let everyday = newBeginnings();
 let dreams = document.getElementById("dreams");
 document.body.addEventListener("keyup", myDream)
-function myDream () {
+function myDream() {
   count++
   if (dreams.innerHTML === "a new tomorrow") {
     dreams.innerHTML = "create a better today";
