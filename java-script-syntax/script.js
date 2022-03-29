36a2294bfd48e8d51418cd6fa1d13122711cb51d
kellyn nettles <92842273+nellynkettles@users.noreply.github.com>
2022-03-29 01:28:24 -0400

    javascript syntax

---
+++ java-script-syntax/script.js
@@ -0,0 +1,16 @@
+let count = 0
+let everyday = newBeginnings ();
+let dreams = document.getElementById("dreams");
+document.body.addEventListener("keyup", myDream)
+function myDream () {
+  count++
+  if (dreams.innerHTML === "a new tomorrow") {
+    dreams.innerHTML = "a better today";
+    dreams.innerHTML = "imagined reality";
+    dreams.innerHTML = "is life but this?"
+    dreams.innerHTML = "the marked ending of a new beginning"
+  } else if (count >=5){
+    dreams.innerHTML = "to dream constantly is to think radically"
+  }
+
+}
